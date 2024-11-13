7ff0382eeafc23b7a3ea17df9bafaab581b14702
董柏霖 <bolin20090402@163.com>
2024-11-13 16:37:38 +0800

    小修改

--- PODSK/renderer.js
+++ PODSK/renderer.js
@@ -1,5 +1,5 @@
 const information = document.getElementById('info')
-information.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`
+//information.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`
 
 const func = async () => {
     const response = await window.versions.ping()
